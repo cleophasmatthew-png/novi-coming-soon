b1777d79de886eb67b82ac5b9f4bd0f98cb265e6
Your Name <youremail@example.com>
2026-01-20 21:16:26 +0200

    Updates

---
+++ static/js/488.3789e46a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknovi_coming_soon=self.webpackChunknovi_coming_soon||[]).push([[488],{488(n,e,o){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.3789e46a.chunk.js.map
